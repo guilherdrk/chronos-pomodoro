bbe8b051eb3f4742f6eb78c59285d550cd01e1b1
Guilherme Messias <guibem20@gmail.com>
2026-04-24 13:00:01 -0300

    criando o componente de logo com icone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import "./styles/theme.css";
-import "./styles/global.css";
 import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
 
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export const App = () => {
-  console.log("oi");
   return (
     <>
         <Container>
-            <Heading>LOGO</Heading>
-            
+            <Logo />
         </Container>
         <Container>
             <Heading>MENU</Heading>
