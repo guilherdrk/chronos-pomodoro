ecacf2da7ed1922900c5343e4cf8439252e5a78f
Guilherme Messias <guibem20@gmail.com>
2026-04-24 16:12:13 -0300

    Input: componente um pouco mais complexo

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 
-
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
-
+import { DefaultInput } from "./components/DefaultInput";
 
 export const App = () => {
-	return (
-		<>
-			<Container>
-				<Logo />
-			</Container>
-			<Container>
-				<Menu />
-			</Container>
-			<Container>
-				<CountDown />
-			</Container>
-			<Container>
-				<section>FOOTER</section>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="Task: "
+              id="meuInput"
+              type="text"
+            />
+          </div>
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+          <div className="formRow">
+            <button>enviar</button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
 };
