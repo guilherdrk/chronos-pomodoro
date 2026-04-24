2e07012a68d6da90b44e466bff852b0c725326f8
Guilherme Messias <guibem20@gmail.com>
2026-04-24 14:36:57 -0300

    criando componente de CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,24 @@ import { Logo } from "./components/Logo";
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 
 export const App = () => {
-  return (
-    <>
-        <Container>
-            <Logo />
-        </Container>
-        <Container>
-            <Menu />
-        </Container>
-        <Container>
-            <section>FORM</section>
-        </Container>
-        <Container>
-            <section>FOOTER</section>
-        </Container>
-    </>
-  );
+	return (
+		<>
+			<Container>
+				<Logo />
+			</Container>
+			<Container>
+				<Menu />
+			</Container>
+			<Container>
+				<CountDown />
+			</Container>
+			<Container>
+				<section>FOOTER</section>
+			</Container>
+		</>
+	);
 };
