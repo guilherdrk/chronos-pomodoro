f9965ddcc5b4c54267141b925beb240ab1a47406
Guilherme Messias <guibem20@gmail.com>
2026-04-25 17:03:27 -0300

    criando componente de footer(rodapé)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/DefaultInput";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { PlayCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
@@ -38,10 +39,13 @@ export const App = () => {
             <Cycles />
           </div>
           <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} color="green"/>
-            <DefaultButton icon={<StopCircleIcon />} color="red"/>
+            <DefaultButton icon={<PlayCircleIcon />}/>
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer/>
       </Container>
     </>
   );
