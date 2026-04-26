70c4afef22daa2fd2320961b501dd4bb442ea64f
Guilherme Messias <guibem20@gmail.com>
2026-04-26 09:16:47 -0300

    useEffect - hook feito para trabalhar com efeitos colaterais no componente

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,7 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
 import { MenuLink } from "../MenuLink";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type AvailableThemes = "dark" | "light";
 
@@ -10,15 +10,30 @@ export const Menu = () => {
 
   function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
     event.preventDefault();
-    console.log("clicado");
-
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark' ;
-      document.documentElement.setAttribute('data-theme', nextTheme);
+      // document.documentElement.setAttribute('data-theme', nextTheme);
       return nextTheme;
     });
-    
   }
+
+  // useEffect(() => {
+  //   console.log('Use effect sem dependências', Date.now());
+
+  // }); //executado toda vez que o componente renderiza na tela
+
+  // useEffect(() => {
+  //   console.log('useEffect com array de dependencias vazio!', Date.now());
+  // }, []); //executa apenas quando o react monta o componente pela primeira vez
+
+  useEffect(() => {
+    console.log('theme mudou', theme, Date.now());
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('olha este compenente será atualizado');
+    }
+  }, [theme]); //Executa apenas quando o valor de theme muda
 
   return (
     <>
