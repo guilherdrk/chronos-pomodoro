81166c0ed5465b3e027385b6c6d0394074ec1090
Guilherme Messias <guibem20@gmail.com>
2026-05-10 00:42:47 -0300

    web worker com setTimeout + useEffect para contagem regressiva

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/timeWorkerManager";
 
 
 
@@ -10,12 +11,27 @@ type TaskContextProvierProps = {
 };
 
 export function TaskContextProvider({ children }: TaskContextProvierProps) {
-
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const worker = TimerWorkerManager.getInstance();
 
-  useEffect(()=>{
-    console.log(state);
-  }, [state]);
+  worker.onmesssage(e => {
+    const countDownSeconds = e.data;
+    console.log(countDownSeconds);
+    if(countDownSeconds <= 0){
+      console.log('Worker COMPLETED');
+      worker.terminate();
+    }
+    
+  })
+
+  useEffect(() => {
+    if (!state.activeTask) {
+      console.log('Worker terminado por falta de activeTask');
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [worker, state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
