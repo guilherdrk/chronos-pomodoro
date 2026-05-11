f5da8b3be5edfdd1d90dc5014819b03467e9d8f6
Guilherme Messias <guibem20@gmail.com>
2026-05-11 14:06:23 -0300

    React Router - criando um router e o componente ScrollToTop

---
+++ src/routers/MainRouter/index.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router"
+import { Home } from "../../pages/Home"
+import { AboutPomodoro } from "../../pages/AboutPomodoro"
+import { NotFound } from "../../pages/NotFound"
+import { useEffect } from "react";
+
+function ScrollToTop(): null {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth'}); 
+  }, [pathname]);
+
+  return null;
+}
+
+export const MainRouter = (() => {
+
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ScrollToTop/>
+    </BrowserRouter>
+  )
+
+})
